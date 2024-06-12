7e2a3e2b2a799f7a8871a06dd6b0b4cf7b724300
Yash126116 <86655251+Yash126116@users.noreply.github.com>
2024-06-12 12:07:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,11 @@
 import env
 import logging
 from pyrogram import Client, idle
+import pyrogram.utils
 from pyromod import listen  # type: ignore
 from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood, AccessTokenInvalid
 
+pyrogram.utils.channel_id = -1002006327852
 
 logging.basicConfig(level=logging.INFO, encoding="utf-8", format="%(asctime)s - %(levelname)s - \033[32m%(pathname)s: \033[31m\033[1m%(message)s \033[0m")
 
