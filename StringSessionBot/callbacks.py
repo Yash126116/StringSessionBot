436f6605cecaa47ccc0e440d5ed7bfeb1eb23fa0
Yash126116 <86655251+Yash126116@users.noreply.github.com>
2024-06-12 12:47:50 +0530

    Update callbacks.py

--- StringSessionBot/callbacks.py
+++ StringSessionBot/callbacks.py
@@ -69,7 +69,4 @@ async def _callbacks(bot: Client, callback_query: CallbackQuery):
 
 ERROR_MESSAGE = (
     "Oops! An exception occurred! \n\n**Error** : {} "
-    "\n\nPlease visit https://t.me/+-LAXtYnqhO1mY2U9 if this message doesn't contain any "
-    "sensitive information and you if want to report this as "
-    "this error message is not being logged by us!"
 )
